38333a809bc9547d447868cdb157cbd457441d14
Sam Clark <sam.clark1142@gmail.com>
2014-02-21 21:10:07 -0500

    initial template

---
+++ src/MineBall.java
@@ -0,0 +1,16 @@
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MineBall extends JavaPlugin {
+
+    @EventHandler
+    public void onEnable() {
+
+    }
+
+    @EventHandler
+    public void onDisable() {
+
+    }
+
+}
